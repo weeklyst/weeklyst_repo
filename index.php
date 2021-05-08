b2b297ebf8d0e2259b5680cc2638487fd5be26e5
JonasVonDunkelheit <JonasVonDunkelheit@users.noreply.github.com>
2021-05-07 19:44:02 -0500

    Esta es la primera modificación para la generación del nuevo repositorio de Weeklist

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Weeklyst</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/icono.png">
+    <script src="https://kit.fontawesome.com/eac556b4db.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <header class="cabecera">
+      <div class="menu">
+        <div class="logoej">
+          <i class="fas fa-caret-down icon1">Menú</i>
+        </div>
+
+        <!--<ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Sobre nosotros</a></li>
+          <li><a href="#">Contactenos</a></li>
+        </ul>-->
+      </div>
+
+    </header>
+
+    <div class="container">
+      <div class="body-items">
+
+
+        <div class="week-logo">
+          <img src="img/logoweeklyst.png" class="logo">
+        </div>
+
+
+        <div class="botones">
+          <div class="box registro-usuario">
+            <i class="fas fa-user-clock icon"></i>
+             <h4 class="title">Registrarse</h4>
+             <p>Toma tu asistencia rapida y segura</p>
+          </div>
+
+          <div class="box inicio-usuario">
+            <i class="fas fa-user-circle icon"></i>
+            <h4 class="title">Iniciar Sesión</h4>
+            <p>Bienvenido de nuevo</p>
+          </div>
+        </div>
+
+        <div class="slider-image">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+      </div>
+
+      <hr>
+
+      <div class="roles-content">
+
+        <div class="institucion rol">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div class="instructor rol">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div class="aprendiz rol">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+
+
+
+    <footer>
+      ©Página creada por el equipo Weeklyst™ en sus instalaciones de manera remota
+    </footer>
+
+  </body>
+</html>
