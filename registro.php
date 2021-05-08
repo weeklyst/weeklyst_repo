951d61c2b3461b1370ea67b12ea5626ffb28d569
JonasVonDunkelheit <JonasVonDunkelheit@users.noreply.github.com>
2021-05-08 16:32:29 -0500

    Archivo de registro

---
+++ registro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registro</title>
+    <link rel="stylesheet" href="css/registro.css">
+    <link rel="icon" href="img/icono.png">
+    <script src="https://kit.fontawesome.com/eac556b4db.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <header class="cabecera">
+      <nav class="navigation">
+        <div class="menucont">
+          <ul class="menu">
+            <i class="fas fa-caret-down icon1"><a href"#">Menú</a><ul class="submenu">
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Sobre nosotros</a></li>
+              <li><a href="#">Servicios</a></li>
+              <li><a href="#">Contáctenos</a></li>
+              <li><a href="login.php">Iniciar Sesión</a></li>
+              <li><a href="registro.php">Registrarse</a></li>
+            </ul></i>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  </body>
+</html>
